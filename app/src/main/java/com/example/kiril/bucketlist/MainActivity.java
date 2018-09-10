62a72aaef528a071a2b251c804ef02cbab3201ca
kirilvasilev16 <kiril_matematikat@abv.bg>
2018-09-11 00:12:54 +0300

    first upload

---
+++ app/src/main/java/com/example/kiril/bucketlist/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.kiril.bucketlist;
+
+import android.database.Cursor;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
+    EditText editName;
+    EditText editTextId;
+    Button btnAddData;
+    Button btnviewAll;
+    Button btnDelete;
+    Button btnviewUpdate;
+
+    TextToSpeech toSpeech;
+    boolean isInitialised;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        myDb = new DatabaseHelper(this);
+
+        editTextId = (EditText)findViewById(R.id.tbID);
+        editName = (EditText)findViewById(R.id.tbAdd);
+        btnAddData = (Button)findViewById(R.id.bAdd);
+        btnviewAll = (Button)findViewById(R.id.bView);
+        btnviewUpdate= (Button)findViewById(R.id.bEdit);
+        btnDelete= (Button)findViewById(R.id.bDelete);
+        AddData();
+        viewAll();
+        UpdateData();
+        DeleteData();
+    }
+
+    public void DeleteData() {
+        btnDelete.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Integer deletedRows = myDb.deleteData(editTextId.getText().toString());
+                        if(deletedRows > 0)
+                            Toast.makeText(MainActivity.this,"Data Deleted",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this,"Data not Deleted",Toast.LENGTH_LONG).show();
+                        editTextId.getText().clear();
+                        editName.getText().clear();
+                    }
+                }
+        );
+    }
+    public void UpdateData() {
+        btnviewUpdate.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isUpdate = myDb.updateData(editTextId.getText().toString(),
+                                editName.getText().toString());
+                        if(isUpdate == true)
+                            Toast.makeText(MainActivity.this,"Data Update",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this,"Data not Updated",Toast.LENGTH_LONG).show();
+                        editTextId.getText().clear();
+                        editName.getText().clear();
+                    }
+                }
+        );
+    }
+    public  void AddData() {
+        btnAddData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(editName.getText().toString());
+                        if(isInserted == true)
+                            Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this,"Data not Inserted",Toast.LENGTH_LONG).show();
+                        editTextId.getText().clear();
+                        editName.getText().clear();
+                    }
+                }
+        );
+    }
+
+    public void viewAll() {
+        btnviewAll.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = myDb.getAllData();
+                        if(res.getCount() == 0) {
+                            showMessage("Error","Nothing found");
+                            return;
+                        }
+
+                        StringBuffer buffer = new StringBuffer();
+                        buffer.append("ToDo:"+"\n");
+                        while (res.moveToNext()) {
+                            buffer.append("ID: "+ res.getString(0)+"\n");
+                            buffer.append(" --> " + res.getString(1)+"\n");
+                        }
+
+                        showMessage("List",buffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+}
+
